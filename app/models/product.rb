2d5bc5965a059d3798ab2e566871815a87194cb7
Eric Betancourt <eric_betancourt@outlook.com>
2015-02-10 17:49:51 -0500

    debuging

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
-validates :discription, :name, presense: true
+validates :description, :name, presense: true
 validates :price_in_cents, numericality: { only_integer: true, greater_than: 0}
 end
