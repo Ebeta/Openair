56abab8ccdf655563cd3306fa63dae6dd60d093e
Eric Betancourt <eric_betancourt@outlook.com>
2015-02-10 16:55:53 -0500

    debuging validators

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
 validates :discription, :name, presense: true
-vlalidates :price_in_cents, numericality: { only_integer: true, greater_than: 0}
+validates :price_in_cents, numericality: { only_integer: true, greater_than: 0}
 end
