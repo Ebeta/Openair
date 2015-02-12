16e4f0ca9aac2c36655510569cffacae3bd1f799
AlaaMuk <alaa.muk@gmail.com>
2015-02-12 18:54:37 -0500

    Added Icons to navigation bar and search bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,16 @@ validates :price_in_cents, numericality: { only_integer: true, greater_than: 0}
 
 has_many :reviews
 
-def price_conversion
-price_in_dollars = price_in_cents.to_f / 100
-	sprintf("%.2f",price_in_dollars)
-end
+	def price_conversion
+	price_in_dollars = price_in_cents.to_f / 100
+		sprintf("%.2f",price_in_dollars)
+	end
+
+	def self.search(params)
+		if params
+			where(['name LIKE ? OR description LIKE ?', "%#{params}%", "%#{params}%"])
+		else
+			all
+		end
+	end
 end
